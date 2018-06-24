766ec43a9b8d357fa1ec2d70cbcd0fd0873bfea3
Sirisha Marthy <sirisham@amazon.com>
2018-06-24 11:29:56 -0700

    updating app secet key to retrive from environment variable

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ from passlib.hash import pbkdf2_sha256
 from model import Task, User
 
 app = Flask(__name__)
-app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
+#app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
+
+app.secret_key = os.environ.get('SECRET_KEY').encode()
+
+
 
 @app.route('/all')
 def all_tasks():
